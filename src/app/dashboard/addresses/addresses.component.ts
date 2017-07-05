5e7c1fb5fd4353b725dda550c075bbbd11b3f075
Saadat Rizvi <saadatrezvi@gmail.com>
2017-07-05 18:48:53 +0500

    Insert of addresses added

--- src/app/dashboard/addresses/addresses.component.ts
+++ src/app/dashboard/addresses/addresses.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import { Location } from '@angular/common';
 import {Addresses} from './addresses.dto';
 import 'rxjs/add/operator/switchMap';
@@ -17,13 +17,15 @@ import {CoolLocalStorage} from "angular2-cool-storage";
 })
 export class AddressesComponent implements OnInit {
   addresses: Addresses;
-
-
+  isEditEnabled: boolean;
+  tempAddress: Addresses;
+  isAddEnabled: boolean;
 
 
   constructor(
     private addressService: AddressesService,
     private route: ActivatedRoute,
+    private router: Router,
     private localStorage: CoolLocalStorage
   ) {
   }
@@ -32,6 +34,9 @@ export class AddressesComponent implements OnInit {
    // console.log('Address.component.ts');
    // console.log(this.localStorage.getItem('token'));
 
+    this.isEditEnabled=false;
+    this.isAddEnabled=false;
+    this.resetTemp();
     this.addressService.getAddresses(+this.localStorage.getItem('id'))
       .then(addresses =>  this.addresses=addresses);
 
@@ -41,6 +46,49 @@ export class AddressesComponent implements OnInit {
  //      .subscribe(addresses => {
  // //       console.log("addresses: "+addresses);
  //        this.addresses = addresses});
+
+  }
+
+  resetTemp(): void{
+
+    this.tempAddress = new Addresses();
+  }
+
+  enableEdit(): void{
+    this.isEditEnabled = true;
+    this.isAddEnabled=false;
+  }
+
+  disableEdit(): void{
+    this.isEditEnabled=false;
+    //(
+
+     this.router.navigate([this.router.url]);
+     console.log()
+  }
+
+  update(): void{
+    this.addressService.update(this.addresses);
+}
+  enableAdd(): void{
+    this.isAddEnabled = true;
+    this.isEditEnabled=false;
+  }
+  disableAdd(): void{
+    this.isAddEnabled=false;
+    this.resetTemp();
+  }
+  add(): void{
+    let data={
+      street: this.tempAddress.street,
+      city: this.tempAddress.city,
+      country: this.tempAddress.country,
+      type: this.tempAddress.type,
+      EmployeeId:this.localStorage.getItem('id')
+    };
+
+    this.addressService.create(data).then(res=>console.log(res));
+    this.disableAdd();
 
   }
 
